114eceee6231888440adca44682ee24f60cdfad6
eferreyro <emafe7472@gmail.com>
2021-05-23 00:12:18 -0500

    creando las tablas y los modelos de cada tabla

--- database/migrations/2021_05_23_045005_create_subcategories_table.php
+++ database/migrations/2021_05_23_045005_create_subcategories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateSubcategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,17 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('subcategories', function (Blueprint $table) {
             $table->id();
 
             $table->string('name');
             $table->string('slug');
             $table->string('image');
-            $table->string('icon');
-
+            $table->boolean('color');
+            $table->boolean('size');
+            
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
 
             $table->timestamps();
         });
@@ -33,6 +36,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('subcategories');
     }
 }
